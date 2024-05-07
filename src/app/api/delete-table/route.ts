23a5f34ce58495b475e27ee534d2caf535713b93
RazvanBanateanu <banateanu.razvan1@gmail.com>
2024-05-07 13:14:09 +0300

    add routes

--- src/app/api/delete-table/route.ts
+++ src/app/api/delete-table/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   try {
     const result =
-      await sql`CREATE TABLE IF NOT EXISTS Food ( Name varchar(255), Category varchar(255), Ingredients TEXT[], Image varchar(255));`;
+      await sql`DROP TABLE Food;`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
